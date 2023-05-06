8f3fb87ffc820fd2e24cb6df1dae8e8ee167696b
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-05-06 15:51:24 +0530

    anim timing tweak

--- components/Landing/page.js
+++ components/Landing/page.js
@@ -1,20 +1,20 @@
-import BandicootShowcase from "../Slices/LandingPage/BandicootShowcase/bandicootShowcase";
-import BluePrint from "../Slices/LandingPage/BluePrint/blueprint";
-import Hero from "../Slices/LandingPage/Hero/Hero";
-import Launch from "../Slices/LandingPage/Launch/launch";
-import ProductOverview from "../Slices/LandingPage/ProductBriefOverview/productOverview";
-import ProductShowcase from "../Slices/LandingPage/ProductShowcaseSlider/productShowcase";
+// import BandicootShowcase from "../Slices/LandingPage/BandicootShowcase/bandicootShowcase";
+// import BluePrint from "../Slices/LandingPage/BluePrint/blueprint";
+// import Hero from "../Slices/LandingPage/Hero/Hero";
+// import Launch from "../Slices/LandingPage/Launch/launch";
+// import ProductOverview from "../Slices/LandingPage/ProductBriefOverview/productOverview";
+// import ProductShowcase from "../Slices/LandingPage/ProductShowcaseSlider/productShowcase";
 
 
-export default function Page() {
-    return(
-        <>
-        <Hero />
-        <ProductOverview />
-        <BandicootShowcase />
-        <BluePrint />
-        <ProductShowcase />
-        <Launch />
-        </>
-    )
-}
+// export default function Page() {
+//     return(
+//         <>
+//         <Hero />
+//         <ProductOverview />
+//         <BandicootShowcase />
+//         <BluePrint />
+//         <ProductShowcase />
+//         <Launch />
+//         </>
+//     )
+// }
