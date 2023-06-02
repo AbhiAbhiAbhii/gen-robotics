18d2cbd11704b37f76f6cc4cf98d7f4ae00b0073
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-06-02 17:44:05 +0530

    test

--- components/Product[child]/prodBotmShowcase.js
+++ components/Product[child]/prodBotmShowcase.js
@@ -20,7 +20,7 @@ export default function ProductBottomShowcase(){
             item: "Machine Vision"
         },
         {
-            item: "Pnemuaticss"
+            item: "Pnemuatics"
         },
     ]
 
