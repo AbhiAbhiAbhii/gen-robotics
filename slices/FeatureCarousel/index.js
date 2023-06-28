4e563b7bdb46f66870506c908a01707738467024
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-06-28 19:01:08 +0530

    Features autoscroll

--- slices/FeatureCarousel/index.js
+++ slices/FeatureCarousel/index.js
@@ -29,7 +29,13 @@ export default function BluePrint({ slice }) {
 
   // refs
   const saferRef = useRef();
+  
 
+  const [ autoplay, setAutoplay ] = useState(false);
+
+ 
+
+  
   
   useEffect(() => {
     
@@ -49,10 +55,9 @@ export default function BluePrint({ slice }) {
       entries.forEach(entry => {
         if(entry.isIntersecting) {
 
-
+           setAutoplay(true)
 
            document.querySelectorAll(".featureCarousel_AssetDiv").forEach(assetDiv => {  // asset anim
-
               assetDiv.style.transform = 'translateY(0%)';
              })
           
@@ -69,38 +74,99 @@ export default function BluePrint({ slice }) {
             })
           }, 500)
 
+          // setTimeout(() => {
+          //   slideRef.current.swiper.slideTo(1)
+          //   SlideItemIndexColor[0].style.color ="#616161";
+          //   SlideItemIndexColor[1].style.color ="#D1BD55";
+  
+          //   SlideItemTitleColor[0].style.color = "#616161";
+          //   SlideItemTitleColor[1].style.color = "#D1BD55";
+  
+          //   SlideItemDescriptColor[0].style.color = "#616161";
+          //   SlideItemDescriptColor[1].style.color = "#D1BD55";
+  
+          //   SlideItemImage[0].style.opacity = "0";
+          //   SlideItemImage[1].style.opacity = "1";
+          // }, 1500) 
+  
+          // setTimeout(() => {
+          //   slideRef.current.swiper.slideTo(2)
+          //   SlideItemIndexColor[1].style.color ="#616161";
+          //   SlideItemIndexColor[2].style.color ="#D1BD55";
+  
+          //   SlideItemTitleColor[1].style.color = "#616161";
+          //   SlideItemTitleColor[2].style.color = "#D1BD55";
+  
+          //   SlideItemDescriptColor[1].style.color = "#616161";
+          //   SlideItemDescriptColor[2].style.color = "#D1BD55";
+  
+          //   SlideItemImage[1].style.opacity = "0";
+          //   SlideItemImage[2].style.opacity = "1";
+          // }, 2500) 
+  
+          // setTimeout(() => {
+          //   slideRef.current.swiper.slideTo(3)
+          //   SlideItemIndexColor[2].style.color ="#616161";
+          //   SlideItemIndexColor[3].style.color ="#D1BD55";
+  
+          //   SlideItemTitleColor[2].style.color = "#616161";
+          //   SlideItemTitleColor[3].style.color = "#D1BD55";
+  
+          //   SlideItemDescriptColor[2].style.color = "#616161";
+          //   SlideItemDescriptColor[3].style.color = "#D1BD55";
+  
+          //   SlideItemImage[2].style.opacity = "0";
+          //   SlideItemImage[3].style.opacity = "1";
+          // }, 3500) 
+  
+          // setTimeout(() => {
+          //   slideRef.current.swiper.slideTo(0)
+          //   SlideItemIndexColor[3].style.color ="#616161";
+          //   SlideItemIndexColor[0].style.color ="#D1BD55";
+  
+          //   SlideItemTitleColor[3].style.color = "#616161";
+          //   SlideItemTitleColor[0].style.color = "#D1BD55";
+  
+          //   SlideItemDescriptColor[3].style.color = "#616161";
+          //   SlideItemDescriptColor[0].style.color = "#D1BD55";
+  
+          //   SlideItemImage[3].style.opacity = "0";
+          //   SlideItemImage[0].style.opacity = "1";
+          // }, 4500)
+
         }
       })
     }, {threshold: 0.65})
     observerB.observe(document.getElementById("bluePrint"));
 
 
-  },[])
+      
 
-  // const [slideCount, setSlideCount] = useState(0);
+      if(autoplay === true) {
+        // slideRef.current.swiper.autoplay.start()
+        slideRef.current.swiper.autoplay.start()
+
+      } else if( autoplay === false ) {
+        slideRef.current.swiper.autoplay.stop()
+
+        // slideRef.current.swiper.autoplay.stop()
+      }
+
+
+  })
+
+  
+  // const MouseEnter = () => {
+  //   setAutoplay(false)
+  // }
+
+  // const MouseLeave = () => {
+  //   setAutoplay(true)
+  // }
+
 
   const slideRef = useRef();
 
-  // const test = () => {
-
-  //   let slide = document.querySelectorAll('.singleSlide')
-
-  //   let slideA = slide[0];
-  //   let slideB = slide[1];
-  //   let slideC = slide[2];
-
-  //   console.log("slideA", slideA)
-
-  //   slideRef.current.swiper.slideTo(1, 1000, false);
-  //   slideRef.current.swiper.slideTo(0, 1000, false)
-  //   slideA.swiper.slideTo(0,1000, false )
-  //   console.log("slideRef", slideRef.current.swiper.slideTo(1, 1000, false))
-
-  // }
-
-  // const testing = () => {
-  //   slideRef.current.swiperSlideIndex = `${slideRef.current.swiperSlideIndex} + 1`
-  // }
 
   return(
     <section className='bluePrint' id='safe'>
@@ -130,9 +196,9 @@ export default function BluePrint({ slice }) {
                   })
                 }
               </div>
-              <div className='featureCarousel_ContentContainer' id='bluePrint'>
-                <div className='featureCarousel_SliderContainer'>
-                  <Swiper style={{height:'100%'}}
+              <div className='featureCarousel_ContentContainer' id='bluePrint' >
+                <div  className='featureCarousel_SliderContainer'>
+                  <Swiper  style={{height:'100%'}}
                     direction={'vertical'}
                     ref={slideRef}
                       // autoplay={{
@@ -143,16 +209,85 @@ export default function BluePrint({ slice }) {
                     modules={[Autoplay, Mousewheel ]}
                     // mousewheel={true}
                     slidesPerView={2.4}
-                    onSlideChange={(i) => console.log('slide change',i.activeIndex)}
+                    onClick={() => setAutoplay(false)}
+                    onMouseEnter={() => setAutoplay(false)}
+                    onSlideChange={(i) => {
+                      let SlideIndex = document.querySelectorAll('.featureCarousel_Swiper_idDiv');
+                      let SlideTitle = document.querySelectorAll('.featureCarousel_Swiper_TitleDiv');
+                      let SlideDescript = document.querySelectorAll('.featureCarousel_Swiper_DescriptionDiv');
+                      let SlideImage = document.querySelectorAll('.featureCarousel_AssetDiv');
+
+
+                      if( autoplay == true ) {
+                        if(i.activeIndex == 0) {
+
+                          SlideIndex[3].style.color = "#616161"
+                          SlideTitle[3].style.color = "#616161"
+                          SlideDescript[3].style.color = "#616161"
+                          SlideImage[3].style.opacity = "0"
+                          
+                          SlideIndex[0].style.color = "#D1BD55"
+                          SlideTitle[0].style.color = "#D1BD55"
+                          SlideDescript[0].style.color = "#D1BD55"
+                          SlideImage[0].style.opacity = "1"
+  
+                         SlideIndex[1].style.color = "#616161"
+                         SlideTitle[1].style.color = "#616161"
+                         SlideDescript[1].style.color = "#616161"
+                         SlideImage[1].style.opacity = "0"
+  
+                        } else if(i.activeIndex == 1) {
+  
+                          SlideIndex[0].style.color = "#616161"
+                          SlideTitle[0].style.color = "#616161"
+                          SlideDescript[0].style.color = "#616161"
+                          SlideImage[0].style.opacity = "0"
+  
+                          SlideIndex[1].style.color = "#D1BD55"
+                          SlideTitle[1].style.color = "#D1BD55"
+                          SlideDescript[1].style.color = "#D1BD55"
+                          SlideImage[1].style.opacity = "1"
+  
+                        } else if(i.activeIndex == 2) {
+  
+                          SlideIndex[1].style.color = "#616161"
+                          SlideTitle[1].style.color = "#616161"
+                          SlideDescript[1].style.color = "#616161"
+                          SlideImage[1].style.opacity = "0"
+  
+  
+  
+                          SlideIndex[2].style.color = "#D1BD55"
+                          SlideTitle[2].style.color = "#D1BD55"
+                          SlideDescript[2].style.color = "#D1BD55"
+                          SlideImage[2].style.opacity = "1"
+                          setTimeout(() => {
+                            SlideIndex[2].style.color = "#616161"
+                            SlideTitle[2].style.color = "#616161"
+                            SlideDescript[2].style.color = "#616161"
+                            SlideImage[2].style.opacity = "0"
+  
+  
+                            SlideIndex[3].style.color = "#D1BD55"
+                            SlideTitle[3].style.color = "#D1BD55"
+                            SlideDescript[3].style.color = "#D1BD55"
+                            SlideImage[3].style.opacity = "1"
+                          }, 1800)
+                        } 
+                      }
+                      
+                    }}
+
                     onSwiper={(i) => console.log(i, "hiiii")} 
                     > 
                     {
                       slice.items.map((data, i) => {
                         return(
-                          <SwiperSlide 
+                          <SwiperSlide  
                           // onClick={() =>setSlideCount(i,console.log("SET SLIDE COUNT", i) )} 
                           className='singleSlide'
                           onClick={() => {
+                            setAutoplay(false)
                             if(i === 0)  {
                               slideRef.current.swiper.slideTo(0, 500, `${quint}`)
                             } else if(i === 1) {
@@ -163,11 +298,13 @@ export default function BluePrint({ slice }) {
                               slideRef.current.swiper.slideTo(2, 500, `${quint}`)
                             }
                           }}
+
+                          onMouseEnter={() => setAutoplay(false)}
                             key={i} 
                             style={{cursor:'grab',scrollSnapType:'x mandatory',scrollPadding:'0 24px', height:'fit-content', transition:'all 0.2s cubic-bezier(0.85, 0, 0.15, 1)'}}>
-                              <div className='featureCarousel_Swiper_TextContainer' onClick={() => setImage(i)} >
+                              <div className='featureCarousel_Swiper_TextContainer' onClick={() => setImage(i)}>
                                 <div className='featureCarousel_Swiper_idDiv' 
-                                  style={{color: image == i ? '#D1BD55':'#616161', opacity:'0', transition:'all 0.8s cubic-bezier(0.85, 0, 0.15, 1)'}}>
+                                  style={{color: image == i ? '#D1BD55':'#616161', opacity: '0', transition:'all 0.8s cubic-bezier(0.85, 0, 0.15, 1)'}}>
                                   <p>
                                     { data.blue_print_id[0].text }
                                   </p>
