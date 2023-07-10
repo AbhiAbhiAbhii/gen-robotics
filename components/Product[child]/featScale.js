8de335d2fe310250c3d0dc1d64b7d89ca8e31531
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-07-10 09:30:34 +0530

    product page discover section

--- components/Product[child]/featScale.js
+++ components/Product[child]/featScale.js
@@ -1,9 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { useSwiper } from 'swiper/react';
+import { Autoplay,Navigation, Pagination, Scrollbar, A11y, Mousewheel } from 'swiper';
+import 'swiper/css';
+import "swiper/css/free-mode";
+import 'swiper/css/navigation';
 
 import Image from "next/image"
+import { useState } from 'react';
 
 
 export default function FeaturesScale({ FTitleA, FTitleB}){
+
+    const [ swipeProgress, setSwipeProgress ] = useState(0);
 
     let featShowcase = [
         {
@@ -32,9 +41,13 @@ export default function FeaturesScale({ FTitleA, FTitleB}){
         },
     ]
 
+    let quart = 'cubic-bezier(0.76, 0.00, 0.24, 1.00)';
+
+
+
     return(
         <>
-        <section className="featureScale" style={{margin:'8em 0'}}>
+        <section className="featureScale">
             <div className="featureScale_Container">
                 <div className="featureScale_TitleContainer">
                     <div className="featureScale_Title">
@@ -46,36 +59,94 @@ export default function FeaturesScale({ FTitleA, FTitleB}){
                 </div>
                 <div className="featureScale_Showcase">
                     <div className="featureScale_Showcase_Container">
-                        {
-                            featShowcase.map((data, i) => {
-                                return(
-                                    <div className="featureScale_Showcase_Item" key={i} style={{display:'flex', flexDirection: i % 2 == 0 ? 'row':'row-reverse', margin:'12em 0',}}>
-                                        <div className="featureScale_Showcase_Item_TextContainer" style={{display:'flex', flexDirection:'column', position:'relative', transform: i % 2 == 0 ? '':'translateX(18%)'}}>
-                                            <div className="featureScale_Showcase_Item_Index">
-                                                <p>
-                                                    { data.index }
-                                                </p>
+                        {/* Desktop Component */}
+                        <div className="featureScale_Showcase_Item_Container">
+                            {
+                                featShowcase.map((data, i) => {
+                                    return(
+                                        <div className="featureScale_Showcase_Item" key={i} style={{display:'flex', flexDirection: i % 2 == 0 ? 'row':'row-reverse', margin:'12em 0',}}>
+                                            <div className="featureScale_Showcase_Item_TextContainer" style={{display:'flex', flexDirection:'column', position:'relative', transform: i % 2 == 0 ? '':'translateX(18%)'}}>
+                                                <div className="featureScale_Showcase_Item_Index">
+                                                    <p>
+                                                        { data.index }
+                                                    </p>
+                                                </div>
+                                                <div className="featureScale_Showcase_Item_Title">
+                                                    <p>
+                                                        { data.title }
+                                                    </p>
+                                                </div>
+                                                <div className="featureScale_Showcase_Item_Desc">
+                                                    <p>
+                                                        { data.description }
+                                                    </p>
+                                                </div>
                                             </div>
-                                            <div className="featureScale_Showcase_Item_Title">
-                                                <p>
-                                                    { data.title }
-                                                </p>
-                                            </div>
-                                            <div className="featureScale_Showcase_Item_Desc">
-                                                <p>
-                                                    { data.description }
-                                                </p>
+                                            <div className="featureScale_Showcase_Item_AssetContainer" style={{justifyContent: i % 2 == 0 ? 'flex-end':'flex-start'}}>
+                                                <div className="featureScale_Showcase_Item_AssetBg">
+                                                    <img src={data.src} alt="asset" />
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="featureScale_Showcase_Item_AssetContainer" style={{justifyContent: i % 2 == 0 ? 'flex-end':'flex-start'}}>
-                                            <div className="featureScale_Showcase_Item_AssetBg">
-                                                <img src={data.src} alt="asset" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </div>
+                        {/* Desktop Component End */}
+
+                        {/* Mobile Component */}
+                            <div className="featureScale_Showcase_Item_Container_Mob">
+                                <Swiper
+                                    slidesPerView={1}
+                                    spaceBetween={50}
+                                    onSlideChange={(i) => setSwipeProgress(i.activeIndex)}
+                                >
+                                    {
+                                        featShowcase.map((data, i) => {
+                                            return(
+                                                <SwiperSlide key={i}>
+                                                    <div className='featureScale_Showcase_Item_Mob'>
+                                                        <div className='featureScale_Showcase_Item_Mob_Left'>
+                                                            <div className='featureScale_Showcase_Item_Index'>
+                                                                <p>
+                                                                    { data.index }
+                                                                </p>
+                                                            </div>
+                                                            <div className='featureScale_Showcase_Item_Title'>
+                                                                <p>
+                                                                    { data.title }
+                                                                </p>
+                                                            </div>
+                                                            <div className='featureScale_Showcase_Item_Desc'>
+                                                                <p>
+                                                                    { data.description }
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                        <div className='featureScale_Showcase_Item_Mob_Right'>
+                                                            <div className='featureScale_Showcase_Item_AssetBg'>
+                                                                <img src={ data.src } alt='asset' />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </SwiperSlide>
+                                            )
+                                        })
+                                    }
+                                </Swiper>
+                                <div className='prodProgress_Mob'>
+                                    <div style={{
+                                        background:'rgba(146, 112, 44, 1)',
+                                        height:'100%',
+                                        width: swipeProgress == 0 ? '20%' : 
+                                        swipeProgress == 1 ? '50%' :
+                                        swipeProgress == 2 ? '75%' :
+                                        swipeProgress == 3 ? '100%' : null,
+                                        transition: `all 0.3s ${quart}` 
+                                    }} />
+                                </div>
+                            </div>
+                        {/* Mobile Component End */}
                     </div>
                 </div>
             </div>
