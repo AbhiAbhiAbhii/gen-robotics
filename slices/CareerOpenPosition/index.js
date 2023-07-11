4b2162570609ee7a737758e345819f1942097769
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-07-11 20:08:40 +0530

    career mob

--- slices/CareerOpenPosition/index.js
+++ slices/CareerOpenPosition/index.js
@@ -22,22 +22,25 @@ export default function CareerOpenPosition({slice}){
             slice.items.map((data, i) => {
               return(
                 <div className="carrOP_Item" key={i}>
-                  <div className="carrOP_Item_Position">
-                    <p>
-                      <PrismicRichText field={data.position_title} />
-                    </p>
-                  </div>
-                  <div className="carrOP_Item_Position_Type">
-                    <p>
-                      <PrismicRichText field={data.position_type} />
-                    </p>
+                  <div className="carrOP_Item_Inner_Container">
+                    <div className="carrOP_Item_Position">
+                      <p>
+                        <PrismicRichText field={data.position_title} />
+                      </p>
+                    </div>
+                    <div className="carrOP_Item_Position_Type">
+                      <p>
+                        <PrismicRichText field={data.position_type} />
+                      </p>
+                    </div>
                   </div>
                   <PrismicLink className="carrOP_Item_Link" field={data.cta_link}>
                     <div className="carrOP_Item_CTA">
                       <div className="carrOP_Item_InnerCTA_WRAP">
                         <button
                         style={{background:'none', outline:'none', border:'none', cursor:'pointer'}}
-                        >Apply Now</button><span style={{position:'relative', transform:'translate(5%, 6%)'}}><img src="/OpenPosition/OP_Arrow.svg" alt="arrow" /></span>
+                        >Apply Now</button><span style={{position:'relative', transform:'translate(5%, 6%)'}}>
+                          <img src="/OpenPosition/OP_Arrow.svg" alt="arrow" /></span>
                       </div>
                     </div>
                   </PrismicLink>
