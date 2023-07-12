57b14cbd3233b17e0efd1d5a4ea259fb7a268d4b
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-07-12 12:36:52 +0530

    test

--- components/Product[child]/prodHero.js
+++ components/Product[child]/prodHero.js
@@ -9,7 +9,7 @@ export default function ProductHero({ video, img, title }){
     return(
         <section className="product">   
             <div className="product_HeroImg">
-                <video autoPlay="true" muted="true" loop="true" playsInline="true" height="100%" width="100%" style={{display: video ? 'block':'none' , height:'100%', width: '100%', objectFit:'cover'}}>
+                <video autoPlay="true" muted="true" preload="auto" loop="true" playsInline="true" height="100%" width="100%" style={{display: video ? 'block':'none' , height:'100%', width: '100%', objectFit:'cover'}}>
                     <source src={video}  type="video/mp4" />
                 </video>
                 <img style={{display: img ? '':'none', height:'100%', width:'100%'}} src={img} alt="" />
