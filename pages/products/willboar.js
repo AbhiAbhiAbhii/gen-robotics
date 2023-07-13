1edfa160980377809b6931940d55091a7d96b379
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-07-13 19:18:36 +0530

    test

--- pages/products/willboar.js
+++ pages/products/willboar.js
@@ -544,7 +544,7 @@ export default function Willboar() {
           <ProductOverviewEB overviewEyebrow={overviewEyebrow} colorText={colorText} ovEbDescA={ovEbDescA} />
           <ProductImgSequence ImgSeqTitle={ImgSeqTitle} texts={texts} frameCount={frameCount} imagelinks={imagelinks} />
           <FeaturesScale featShowcase={featShowcase} FTitleA={ FTitleA } FTitleB={ FTitleB } />
-          <ProductBottomShowcase TitleA={TitleA} TitleB={TitleB} TitleMob={TitleMob} BottomTitle={BottomTitle} info={info} />
+          <ProductBottomShowcase TitleA={TitleA} TitleB={TitleB} TitleMob={TitleMob} BottomTitle={BottomTitle} info={info} video="/Products/Bandicoot_Mini/Splitup Mini Bandicoot 01.mp4" />
           <Footer />
         </>
     )
